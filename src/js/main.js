16911a3e378ee8e11ff98f09a9270f45953f1a4f
hoangviethung <hoangviet.hung.009@gmail.com>
2019-12-29 22:25:47 +0700

    hoàn thành task nhúng dropzone

--- src/js/main.js
+++ src/js/main.js
@@ -300,40 +300,6 @@ function SVG() {
 	});
 }
 
-// UPLOAD FILE
-function uploadFile() {
-	var template = document.querySelector("#template");
-	if (template) {
-		var templateNew = template.parentNode.innerHTML;
-		o.parentNode.removeChild(template);
-		var itemDropZone = new Dropzone(".upload-file", {
-			url: "/upload",
-			thumbnailWidth: 80,
-			thumbnailHeight: 80,
-			parallelUploads: 20,
-			previewTemplate: templateNew,
-			autoQueue: !1,
-			previewsContainer: "#previews",
-			clickable: ".fileinput-button"
-		});
-		i.on("addedfile", (function(e) {
-			e.previewElement.querySelector(".start").onclick = function() {
-				itemDropZone.enqueueFile(e)
-			}
-		})), itemDropZone.on("totaluploadprogress", (function(e) {
-			document.querySelector("#total-progress .progress-bar").style.width = e + "%"
-		})), itemDropZone.on("sending", (function(e) {
-			document.querySelector("#total-progress").style.opacity = "1", e.previewElement.querySelector(".start").setAttribute("disabled", "disabled")
-		})), itemDropZone.on("queuecomplete", (function(e) {
-			document.querySelector("#total-progress").style.opacity = "0"
-		})), document.querySelector("#actions .start").onclick = function() {
-			itemDropZone.enqueueFiles(i.getFilesWithStatus(Dropzone.ADDED))
-		}, document.querySelector("#actions .cancel").onclick = function() {
-			itemDropZone.removeAllFiles(!0)
-		}
-	}
-}
-
 function checkboxAllRow() {
 	$('input[type-checkbox="all-row"]').on('click', function() {
 		let thisRow = $(this).parents('tr');
@@ -355,8 +321,6 @@ document.addEventListener('DOMContentLoaded', () => {
 	$('[data-toggle="tooltip"]').tooltip();
 	// LOADING
 	loading();
-	// DROPZONE
-	uploadFile();
 	multipleSelect();
 	// TAB
 	const blockMainTab = new Tab(".block-main .tab-container");
