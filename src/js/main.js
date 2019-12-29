211c1323d43faebad97308226bb61a67726f07fc
hoangviethung <hoangviet.hung.009@gmail.com>
2019-12-30 00:26:15 +0700

    no message

--- src/js/main.js
+++ src/js/main.js
@@ -340,9 +340,9 @@ function SVG() {
 function checkboxAllRow() {
 	$('input[type-checkbox="all-row"]').on('click', function() {
 		let thisRow = $(this).parents('tr');
-		thisRow.find('input[type="checkbox"').attr('checked', function(index, attr) {
-			return attr == 'checked' ? null : 'checked';
-		});
+		thisRow.each(function() {
+			$(this).find('label').click();
+		})
 	})
 }
 
